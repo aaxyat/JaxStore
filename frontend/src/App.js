c94ea22d7b3280c159dc0fa3d15733826013aa1d
Ayush Bhattarai <mail@ayushb.com>
2022-11-08 12:04:30 +0545

    add Stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-
+import { Container } from 'react-bootstrap';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-     <h1>Welcome To JaxStore</h1>
+      <Header />
+      <main>
+        <Container>
+        <h1>Welcome To JaxStore</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
