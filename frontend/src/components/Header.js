de500a8cf774084c4ec18d2e196141099b139541
Ayush Bhattarai <mail@ayushb.com>
2022-11-08 13:33:08 +0545

    create navbar and add dummy data

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -9,9 +9,9 @@ const Header = () => {
       <Navbar.Brand href="#home">JaxStore</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="/cart"><i className='fas fa-shopping-cart'>Cart</i></Nav.Link>
-          <Nav.Link href="/login"><i className='fas fa-user'>Sign In</i></Nav.Link>
+        <Nav className="ml-auto">
+          <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+          <Nav.Link href="/login"><i className='fas fa-user'></i>Sign In</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Container>
